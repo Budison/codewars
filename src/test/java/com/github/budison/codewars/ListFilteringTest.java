48616421cbcbc7f1506affe21cdb85587daf8dbf
Kevin Nowak <nowakk96@icloud.com>
2022-09-06 22:32:58 +0200

    update tests for ListFiltering

--- src/test/java/com/github/budison/codewars/ListFilteringTest.java
+++ src/test/java/com/github/budison/codewars/ListFilteringTest.java
@@ -2,7 +2,9 @@ package com.github.budison.codewars;
 
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 import static org.testng.Assert.assertEquals;
@@ -10,10 +12,45 @@ import static org.testng.Assert.assertEquals;
 public class ListFilteringTest {
 
     @Test
-    public void testListFiltering() {
-        assertEquals(List.of(1,2), ListFiltering.filterList(List.of(1,2,"a","b")), formatInput(List.of(1,2,"a","b")));
-        assertEquals(List.of(1,0,15), ListFiltering.filterList(List.of(1,"a","b",0,15)), formatInput(List.of(1,"a","b",0,15)));
-        assertEquals(List.of(1,2,123), ListFiltering.filterList(List.of(1,2,"aasf","1","123",123)), formatInput(List.of(1,2,"aasf","1","123",123)));
+    public void testListFilteringExamples() {
+        assertEquals(List.of(1,2), ListFiltering.filterList(List.of(1,2,"a","b")),
+                formatInput(List.of(1,2,"a","b")));
+        assertEquals(List.of(1,0,15), ListFiltering.filterList(List.of(1,"a","b",0,15)),
+                formatInput(List.of(1,"a","b",0,15)));
+        assertEquals(List.of(1,2,123), ListFiltering.filterList(List.of(1,2,"aasf","1","123",123)),
+                formatInput(List.of(1,2,"aasf","1","123",123)));
+    }
+
+    @Test void testListFilteringExtras() {
+        assertEquals(List.of(), ListFiltering.filterList(List.of("a","b","1")),
+                formatInput(List.of("a","b","1")));
+        assertEquals(List.of(1,2), ListFiltering.filterList(List.of(1,2,"a","b")),
+                formatInput(List.of(1,2,"a","b")));
+    }
+
+    @Test void testListFilteringRandom() {
+        ThreadLocalRandom rand = ThreadLocalRandom.current();
+        for(int i = 0; i < 20; i++) {
+            int size = rand.nextInt(20);
+            List<Object> input = new ArrayList<>();
+            List<Integer> expected = new ArrayList<>();
+            for(int j = 0; j < size; ++j) {
+                if(rand.nextBoolean()) {
+                    int n = rand.nextInt(0, 1000);
+                    input.add(n);
+                    expected.add(n);
+                } else {
+                    if(rand.nextBoolean())
+                        input.add(rand.ints(rand.nextInt(6), 48, 123)
+                                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                                .toString());
+                    else input.add(Integer.toString(rand.nextInt(1000)));
+                }
+            }
+
+            List<Object> result = ListFiltering.filterList(List.copyOf(input));
+            assertEquals(expected, result, formatInput(input));
+        }
     }
 
     private String formatInput(List<Object> input) {
